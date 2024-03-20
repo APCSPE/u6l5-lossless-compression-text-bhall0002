6ed5d3d41ef5caf0e38a959ac5d64b968c1d5beb
bhall0002 <148895473+bhall0002@users.noreply.github.com>
2024-03-20 18:16:23 +0000

    finished

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-#This function..
+#This function performs run length encoding on a string a returns the conpressed string
 def RLE_compress(message):
     characters = list(message)
     past_letter = characters[0]
@@ -20,15 +20,15 @@ def RLE_compress(message):
 
 #TEST CODE
 def test_answers():
-    assert RLE_compress("AABBBAAAABBBBBAAAAAABBBBBBB")) == "A2B3A4B5A6B7"
-    assert RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFF")) == "A2C4B5A7X1F8"
-    assert print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFD")) == "A2C4B5A7X1F8D1"
-    assert print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDD")) == "A2C4B5A7X1F8D2"
-    assert print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDDD")) == "A2C4B5A7X1F8D3"
-    assert print(RLE_compress("ABCDEF")) == "A1B1C1D1E1F1" # as discussed, this one doesn't actually "compress", but it's a good test case
-    assert print(RLE_compress("FFFFFFFFFFFFFFFFFFF")) == "F19"
-    assert print(RLE_compress("F")) == "F1"
-    assert print(RLE_compress("F????")) == "F1?4"
-    assert print(RLE_compress("Mmmmmmmmmm sooooo goooooood!")) == "m1M9 1s1o5 1glo7d1!1"
-    assert print(RLE_compress("Booooooooooooo, hisssssssss")) == "b1o13,1 1h1i1s9"
+    assert RLE_compress("AABBBAAAABBBBBAAAAAABBBBBBB") == "A2B3A4B5A6B7"
+    assert RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFF") == "A2C4B5A7X1F8"
+    assert RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFD") == "A2C4B5A7X1F8D1"
+    assert RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDD") == "A2C4B5A7X1F8D2"
+    assert RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDDD") == "A2C4B5A7X1F8D3"
+    assert RLE_compress("ABCDEF") == "A1B1C1D1E1F1" # as discussed, this one doesn't actually "compress", but it's a good test case
+    assert RLE_compress("FFFFFFFFFFFFFFFFFFF") == "F19"
+    assert RLE_compress("F") == "F1"
+    assert RLE_compress("F????") == "F1?4"
+    assert RLE_compress("Mmmmmmmmmm sooooo goooooood!") == "M1m9 1s1o5 1g1o7d1!1"
+    assert RLE_compress("Booooooooooooo, hisssssssss") == "B1o13,1 1h1i1s9"
 
